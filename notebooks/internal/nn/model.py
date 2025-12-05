a41829198b93b234143fa79697c0f2daadb35ab2
AndreVale69 <sforzini31@gmail.com>
2025-12-05 13:15:19 +0100

    feat(notebooks): add StratifiedKFold

---
+++ notebooks/internal/nn/model.py
@@ -0,0 +1,63 @@
+import torch
+from sklearn.metrics import accuracy_score, f1_score
+from tqdm import tqdm
+
+
+def train_one_epoch(model, loader, optimizer, criterion, device):
+    model.train()
+    running_loss = 0.0
+    all_preds, all_targets = [], []
+
+    for imgs, labels in tqdm(loader, desc="Train", leave=False):
+        imgs = imgs.to(device, non_blocking=True)
+        labels = labels.to(device, non_blocking=True)
+
+        optimizer.zero_grad()
+        logits = model(imgs)
+        loss = criterion(logits, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item() * imgs.size(0)
+
+        preds = logits.argmax(dim=1)
+        all_preds.append(preds.detach().cpu())
+        all_targets.append(labels.detach().cpu())
+
+    all_preds = torch.cat(all_preds).numpy()
+    all_targets = torch.cat(all_targets).numpy()
+
+    epoch_loss = running_loss / len(loader.dataset)
+    acc = accuracy_score(all_targets, all_preds)
+    f1 = f1_score(all_targets, all_preds, average='macro')
+
+    print(f"[] t_loss={epoch_loss:.4f} | F1(macro)={f1:.4f} | Acc={acc:.4f}")
+
+    return epoch_loss, acc, f1
+
+@torch.no_grad()
+def validate(model, loader, criterion, device):
+    model.eval()
+    running_loss = 0.0
+    all_preds, all_targets = [], []
+
+    for imgs, labels in tqdm(loader, desc="Val", leave=False):
+        imgs = imgs.to(device, non_blocking=True)
+        labels = labels.to(device, non_blocking=True)
+
+        logits = model(imgs)
+        loss = criterion(logits, labels)
+        running_loss += loss.item() * imgs.size(0)
+
+        preds = logits.argmax(dim=1)
+        all_preds.append(preds.cpu())
+        all_targets.append(labels.cpu())
+
+    all_preds = torch.cat(all_preds).numpy()
+    all_targets = torch.cat(all_targets).numpy()
+
+    epoch_loss = running_loss / len(loader.dataset)
+    acc = accuracy_score(all_targets, all_preds)
+    f1 = f1_score(all_targets, all_preds, average='macro')
+
+    return epoch_loss, acc, f1
