364e5e2f294b69c88ce0b0589d01ef35bc121786
AndreVale69 <sforzini31@gmail.com>
2025-12-15 18:28:15 +0100

    feat(shuffle_and_cap_bag): optimize instance selection logic for shuffling and capping

--- notebooks-v2/internal/data/shuffle_and_cap_bag.py
+++ notebooks-v2/internal/data/shuffle_and_cap_bag.py
@@ -5,10 +5,9 @@ class ShuffleAndCapBag:
         self.max_instances = max_instances
 
     def __call__(self, x: torch.Tensor) -> torch.Tensor:
-        # x: (n_i, C, H, W)
         n = x.size(0)
-        idx = torch.randperm(n)
-        x = x[idx]
-        if self.max_instances is not None and n > self.max_instances:
-            x = x[:self.max_instances]
-        return x
+        if self.max_instances is None or n <= self.max_instances:
+            idx = torch.randperm(n)
+            return x[idx]
+        idx = torch.randperm(n)[:self.max_instances]  # random subset
+        return x[idx]
